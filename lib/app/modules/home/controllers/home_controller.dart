3b5f812a71e6e93c831972e6a568aa2b277b63d1
Anakkirimanheaven <rizqifauzanhk88@gmail.com>
2025-01-23 13:34:44 +0700

    Flutter berita

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:bismillah/app/modules/dashboard/views/dashboard_view.dart';
+import 'package:bismillah/app/modules/login/views/login_view.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class HomeController extends GetxController {
+
+  late Timer _pindah;
+  final authToken = GetStorage();
+  @override
+  void onInit() {
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => authToken.read('token') == null
+          ? Get.off(
+        () => const LoginView(),
+        transition: Transition.leftToRight,
+      )
+      : Get.off(() => const DashboardView())
+    );
+  }
+
+  @override
+  void onReady() {}
+
+  @override
+  void onClose() {
+    _pindah.cancel();
+  }
+}
